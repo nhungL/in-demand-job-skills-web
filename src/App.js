1fa5547d2621d996399f438b48afb8d27af39403
Nhung Luong <nhungluong@MacBook-Pro-2.local>
2023-06-09 11:35:09 -0700

    connect to apollo client and graphql queries

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_ALL_JOBS = gql`
+  query getAllJobs {
+    getAllJobs{
+        id
+        title
+        skills
+    }
+  }
+`;
+
+function DisplayJobs() {
+  const { loading, error, data } = useQuery(GET_ALL_JOBS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.getAllJobs.map((job) => (
+    <div key={job.id}>
+      <h3>{job.title}</h3>
+      <br />
+      <p>Skills: {job.skills.join(', ')}</p>
+      <br />
+    </div>
+  ));
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h2>My first Apollo app 🚀</h2>
         </a>
+        <DisplayJobs />
       </header>
     </div>
   );
